417b0b1ba628df710d0556ec1f3b5ec9fb72691a
Destiny Lewis <70827545+destiny-lewis@users.noreply.github.com>
2021-12-02 15:20:47 -0800

    Display selected characters in list
    - Check if characters have been selected.
    
    - Iterate through character data and compare with selected characters, if character was selected fill divs with appropriate information for that character.

--- backend.php
+++ backend.php
@@ -1,4 +1,5 @@
 <?php
+
     // Connect to database & collect data.
     if ($dbconnect = mysqli_connect('localhost', 'root', '', 'simpsons_characters')) {
         // connection successful
@@ -13,6 +14,7 @@
             }
             // convert php array into JSON string
             $jsonChar = json_encode($charsArray);
+            $charData = json_decode($jsonChar, true);
             
         } else {
             print "<p>Could not fetch data</p>";
@@ -20,21 +22,48 @@
     } else {
         // connection unsuccessful
         die("Error " . mysqli_error($dbconnect));
-    }
-
-    if (isset($_GET['characters'])) {
-        $characters = $_GET['characters'];
-        $charData = json_decode($jsonChar, true);
-        for ($i = 0; $i < count($charData); $i++) {
-            if (in_array($charData[$i]["Name"], $characters)) {
-                var_dump($charData[$i]["Name"]);
-            }
-        }
-
-        
-        
-    } else {
-        echo "Please select a character";
-    }
-
+    }     
+    
 ?>
+
+<?php if (isset($_GET['characters'])) : ?>
+    <?php for ($i = 0; $i < count($charData); $i++) : ?>
+        <?php if (in_array($charData[$i]['Name'], $_GET['characters'])) : ?>
+            <li class="characters__itemContainer">
+                <div class="characters__item">
+                    <img src="images/<?=$charData[$i]['Image']?>" alt="<?=$charData[$i]['Name']?>" class="characters__image">
+                    <div class="characters__info">
+
+                        <h3 class="characters__name">
+                            <?=$charData[$i]['Name']?>
+                        </h3>
+
+                        <div class="characters__age characters__attribute">
+                            <b>Age:</b> <?=$charData[$i]['Age']?>                                                
+                        </div>
+
+                        <div class="characters__occupation characters__attribute">
+                            <b>Occupation:</b> <?=$charData[$i]['Occupation']?>                                                    
+                        </div>                                                    
+                                                            
+                        <div class="characters__voicedBy characters__attribute">
+                            <b>Voiced by:</b> <?=$charData[$i]['VoiceActor']?>                                                    
+                        </div>                                                    
+                                                            
+                    </div>
+
+                </div>
+            </li>
+        <?php endif; ?>
+    <?php endfor; ?>
+<?php else : ?>
+    <p>Please select some characters!</p>
+<?php endif ?>
+
+
+
+
+
+
+
+
